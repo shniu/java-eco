d85ae98ae3e817660ccf94f53cd23952c641069e
shniu <niushaohan@digcredit.com>
2019-06-04 23:41:22 +0800

    add graceful shutdown

--- spring-boot-graceful-shutdown/src/main/java/io/github/shniu/springbootgracefulshutdown/config/GracefulShutdownConfig.java
+++ spring-boot-graceful-shutdown/src/main/java/io/github/shniu/springbootgracefulshutdown/config/GracefulShutdownConfig.java
@@ -1,8 +1,8 @@
 package io.github.shniu.springbootgracefulshutdown.config;
 
-import com.github.timpeeters.boot.shutdown.autoconfigure.GracefulShutdownAutoConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.Connector;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
@@ -11,7 +11,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextClosedEvent;
 
-// @Configuration
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * https://github.com/spring-projects/spring-boot/issues/4657
+ */
+
+@ConditionalOnProperty(havingValue = "false", prefix = "graceful.shutdown", name = "enabled")
+@Configuration
 @Slf4j
 public class GracefulShutdownConfig {
 
@@ -34,14 +43,45 @@ public class GracefulShutdownConfig {
     private static class GracefulShutdown implements TomcatConnectorCustomizer,
             ApplicationListener<ContextClosedEvent> {
 
+        private volatile Connector connector;
+
         @Override
         public void customize(Connector connector) {
             log.info("=========== customize");
+            this.connector = connector;
         }
 
         @Override
         public void onApplicationEvent(ContextClosedEvent contextClosedEvent) {
+            if (connector == null) {
+                return;
+            }
+
+            stopAcceptingNewRequest();
+            shutdownThreadPoolExecutor();
+        }
+
+        private void shutdownThreadPoolExecutor() {
             log.info("=========== onApplicationEvent");
+            Executor executor = this.connector.getProtocolHandler().getExecutor();
+            if (executor instanceof ThreadPoolExecutor) {
+                try {
+                    ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
+                    threadPoolExecutor.shutdown();
+                    if (!threadPoolExecutor.awaitTermination(30, TimeUnit.SECONDS)) {
+                        log.warn("Tomcat thread pool did not shut down gracefully within "
+                                + "30 seconds. Proceeding with forceful shutdown");
+                    }
+                }
+                catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+
+        private void stopAcceptingNewRequest() {
+            connector.pause();
+            log.info("Paused {} to stop accepting new request.", connector);
         }
     }
 }
