db8a0fc363b314678c3210ff923cc2343b90a5d2
shniu <xiaoniu198903@126.com>
2019-08-08 17:29:59 +0800

    security

--- spring-boot-modular/src/main/java/io/github/shniu/App.java
+++ spring-boot-modular/src/main/java/io/github/shniu/App.java
@@ -1,13 +1,22 @@
 package io.github.shniu;
 
+import io.github.shniu.spi.RedisDriver;
+
+import java.util.Optional;
+import java.util.ServiceLoader;
+import java.util.stream.StreamSupport;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        ServiceLoader<RedisDriver> drivers = ServiceLoader.load(RedisDriver.class);
+        // Optional<RedisDriver> driver = StreamSupport.stream(drivers.spliterator(), false).findFirst();
+        // driver.ifPresent(rd -> System.out.println(rd.gerDriverName()));
+
+        StreamSupport.stream(drivers.spliterator(), false)
+                .forEach(redisDriver -> System.out.println(redisDriver.gerDriverName()));
     }
 }
---
+++ spring-boot-modular/src/main/java/io/github/shniu/spi/RedisDriver.java
@@ -0,0 +1,9 @@
+package io.github.shniu.spi;
+
+/**
+ * @author shniu
+ * @date 2019/08/08 17:00:30
+ */
+public interface RedisDriver {
+    String gerDriverName();
+}
