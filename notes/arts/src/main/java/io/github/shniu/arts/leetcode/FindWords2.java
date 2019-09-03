fa31c42f3c81647508e947a45400c556ad010546
shniu <niushaohan@digcredit.com>
2019-09-04 00:10:33 +0800

    3Sum Java 实现

---
+++ notes/arts/src/main/java/io/github/shniu/arts/leetcode/FindWords2.java
@@ -0,0 +1,9 @@
+package io.github.shniu.arts.leetcode;
+
+import java.util.List;
+
+public class FindWords2 {
+    public List<String> findWords(char[][] board, String[] words) {
+        return null;
+    }
+}
---
+++ notes/arts/src/test/java/io/github/shniu/arts/leetcode/ThreeSumSolutionTest.java
@@ -0,0 +1,28 @@
+package io.github.shniu.arts.leetcode;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+@Slf4j
+public class ThreeSumSolutionTest {
+    private ThreeSumSolution threeSumSolution;
+
+    @Before
+    public void setUp() {
+        threeSumSolution = new ThreeSumSolution();
+    }
+
+    @Test
+    public void testThreeSum() {
+        int[] nums = {0, -1, 1, -1, 2, 1, 1, 3};
+        List<List<Integer>> res = threeSumSolution.threeSum(nums);
+        // 0 -1 1
+        // -1 -1 2
+        log.info("{}", res);
+        Assert.assertEquals(2, res.size());
+    }
+}
