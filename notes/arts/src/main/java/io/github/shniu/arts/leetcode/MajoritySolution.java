2299ff935f76c230ca207c15cb51244cd076f383
shniu <niushaohan@digcredit.com>
2019-09-07 00:45:48 +0800

    majority element

--- notes/arts/src/main/java/io/github/shniu/arts/leetcode/MajoritySolution.java
+++ notes/arts/src/main/java/io/github/shniu/arts/leetcode/MajoritySolution.java
@@ -3,6 +3,8 @@ package io.github.shniu.arts.leetcode;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
 
 public class MajoritySolution {
     public int majorityElement(int[] nums) {
@@ -10,6 +12,37 @@ public class MajoritySolution {
         return sort(nums);
         // return loopUntil(nums);
         // return boyerMooreVote(nums);
+    }
+
+    // 随机法
+    private int randomSelect(int[] nums) {
+        Random random = new Random();
+        int majorityCnt = nums.length / 2;
+
+        while (true) {
+            int candidate = nums[randChoosePos(random, 0, nums.length)];
+            if (countOccurences(nums, candidate) > majorityCnt) {
+                return candidate;
+            }
+        }
+    }
+
+    private int countOccurences(int[] nums, int candidate) {
+        /*int cnt = 0;
+        for (int num : nums) {
+            if (candidate == num) {
+                cnt += 1;
+            }
+        }
+        return cnt;*/
+        return (int) Arrays.stream(nums)
+                .filter(num -> candidate == num)
+                .count();
+
+    }
+
+    private int randChoosePos(Random random, int min, int max) {
+        return random.nextInt(max - min) + min;
     }
 
     // hash 法
@@ -23,9 +56,22 @@ public class MajoritySolution {
             counts.put(num, counts.get(num) + 1);
         }
 
+        /*for (Iterator<Map.Entry<Integer, Integer>> iterator =counts.entrySet().iterator(); iterator.hasNext();) {
+            Map.Entry<Integer, Integer> entry = iterator.next();
 
+            entry.getKey();
+            entry.getValue();
+        }*/
 
-        return 0;
+        Map.Entry<Integer, Integer> majority = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (majority == null || entry.getValue() > majority.getValue()) {
+                majority = entry;
+            }
+        }
+
+        Objects.requireNonNull(majority);
+        return majority.getKey();
     }
 
     // 排序法
