dc9c9f7496e2f621d8179e1ad95e7371ef2f125e
shniu <niushaohan@digcredit.com>
2019-12-19 18:50:39 +0800

    Test CAS and CountDownLatch/CyclicBarrier

---
+++ notes/juc/src/main/java/io/github/shniu/juc/atomic/Add10K.java
@@ -0,0 +1,52 @@
+package io.github.shniu.juc.atomic;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicLong;
+
+// 线程安全的 add10K
+public class Add10K {
+    private AtomicLong count = new AtomicLong(0);
+
+    public void add10K() {
+        int i = 0;
+
+        while (i++ < 10000) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            count.getAndIncrement();
+        }
+    }
+
+    public long getCount() {
+        return count.get();
+    }
+
+    public static void main(String[] args) {
+        Add10K test = new Add10K();
+
+        CountDownLatch latch = new CountDownLatch(3);
+        new Thread(() -> {
+            test.add10K();
+            latch.countDown();
+        }).start();
+        new Thread(() -> {
+            test.add10K();
+            latch.countDown();
+        }).start();
+        new Thread(() -> {
+            test.add10K();
+            latch.countDown();
+        }).start();
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(test.getCount()); // 30000
+    }
+}
---
+++ notes/juc/src/main/java/io/github/shniu/juc/demo/orderverify/v2/VerifyOrder.java
@@ -0,0 +1,60 @@
+package io.github.shniu.juc.demo.orderverify.v2;
+
+import java.util.List;
+
+public class VerifyOrder {
+    List<Order> proOrders;
+    List<Order> deliveryOrders;
+
+    public void checkAllOrders() {
+        while (hasUncheckOrder()) {
+            Thread t1 = new Thread(() -> {
+                proOrders = getProOrders();
+            });
+
+            Thread t2 = new Thread(() -> {
+                deliveryOrders = getDelivyOrders();
+            });
+            t1.start();
+            t2.start();
+
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            List<Order> diff = check(proOrders, deliveryOrders);
+            save(diff);
+        }
+    }
+
+    private void save(List<Order> diff) {
+        // todo
+    }
+
+    private List<Order> check(List<Order> proOrders, List<Order> deliveryOrders) {
+        // todo
+        return null;
+    }
+
+    private List<Order> getDelivyOrders() {
+        // todo
+        return null;
+    }
+
+    private List<Order> getProOrders() {
+        // todo
+        return null;
+    }
+
+    private boolean hasUncheckOrder() {
+        // todo
+        return false;
+    }
+}
+
+class Order {
+    // todo
+}
