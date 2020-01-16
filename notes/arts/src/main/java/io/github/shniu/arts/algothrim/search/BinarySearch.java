c321cb5d78fc8336cf27be9c6c37129f30312a77
shniu <niushaohan@digcredit.com>
2020-01-16 15:47:44 +0800

    二分

--- notes/arts/src/main/java/io/github/shniu/arts/algothrim/search/BinarySearch.java
+++ notes/arts/src/main/java/io/github/shniu/arts/algothrim/search/BinarySearch.java
@@ -2,43 +2,41 @@ package io.github.shniu.arts.algothrim.search;
 
 public class BinarySearch {
 
-    public static int search(int[] arr, int value) {
+    public static int search(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
 
-        int low = 0;
-        int high = arr.length - 1;
-
+        // 这里的中止条件是 low <= high, 因为 high = arr.length - 1
         while (low <= high) {
             // 使用 low + (high - low) / 2, 而不使用 (high + low) / 2, 是因为 high + low 可能造成溢出
-            // int mid = low + (high - low) / 2;
+            // int mid = low + (high - low) / 2;  // 这种方式是可以的，不如位运算效率高
             int mid = low + ((high - low) >> 1);  // 这种方式是最优的，效率最高
 
-            if (arr[mid] == value) {
+            if (arr[mid] == target) {
                 return mid;
-            } else if (arr[mid] > value) {
+            } else if (arr[mid] > target) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-
         return -1;
     }
 
     // 递归实现
-    public static int search2(int[] arr, int value) {
-        return bsearch(arr, 0, arr.length - 1, value);
+    public static int search2(int[] arr, int target) {
+        return bsearch(arr, 0, arr.length - 1, target);
     }
 
-    private static int bsearch(int[] arr, int low, int high, int value) {
+    private static int bsearch(int[] arr, int low, int high, int target) {
         if (low > high) return -1;
 
         int mid = low + ((high - low) >> 1);
-        if (arr[mid] == value) {
+        if (arr[mid] == target) {
             return mid;
-        } else if (arr[mid] > value) {
-            return bsearch(arr, low, mid - 1, value);
+        } else if (arr[mid] > target) {
+            return bsearch(arr, low, mid - 1, target);
         } else {
-            return bsearch(arr, mid + 1, high, value);
+            return bsearch(arr, mid + 1, high, target);
         }
     }
 
