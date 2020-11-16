d41a7170dbb97e59fde019b9ae8f18e34ce014b5
niushaohan <shaohan.niu@dfgroup.pro>
2020-11-16 10:05:21 +0800

    Add Concurrency Pattern: Active Object

---
+++ eco-concurrency/src/main/java/io/github/shniu/concurrency/pattern/activeobject/sample1/ActiveObjectFactory.java
@@ -0,0 +1,11 @@
+package io.github.shniu.concurrency.pattern.activeobject.sample1;
+
+/**
+ * @author niushaohan
+ * @date 2020/11/16 00
+ */
+public class ActiveObjectFactory {
+    public static ActiveObject create() {
+        return new DefaultActiveObject();
+    }
+}
